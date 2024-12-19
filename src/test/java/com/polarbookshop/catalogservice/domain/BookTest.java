52c592e26b2a81e9665ed896c69d77141d1dd19b
UPENDRA\upend <upen28.verma@gmail.com>
2024-12-19 14:16:04 +0530

    test caser for catalog-service

---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookTest.java
@@ -0,0 +1,24 @@
+package com.polarbookshop.catalogservice.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookTest {
+
+	@Test
+	public void testBook() {
+
+	}
+
+	@Test
+	public void testBook2() {
+
+	}
+
+	@Test
+	public void testBook3() {
+
+	}
+
+}
